119968a860e0e082ccbe24c93363ee132109fe1f
IRIS <iris@MacBook-Air-de-Cristina.local>
2020-10-12 12:57:10 +0200

    Updates

---
+++ precache-manifest.54c734306e78cbbd91c344ec02901f35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68152a7df27fde83e19af8f1f4928e3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33b0ca79ecad745c6a83",
+    "url": "/static/css/main.58896bcc.chunk.css"
+  },
+  {
+    "revision": "dddd58b87ea79f36468a",
+    "url": "/static/js/2.cde9239f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cde9239f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b0ca79ecad745c6a83",
+    "url": "/static/js/main.32c76bf9.chunk.js"
+  },
+  {
+    "revision": "e2fee617b0cce52d38a2",
+    "url": "/static/js/runtime-main.fde3a231.js"
+  }
+]);
