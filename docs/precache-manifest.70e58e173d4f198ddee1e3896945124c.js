e69cfc544b30af26dcb05abff790a0e03ae40372
Cris <cristina.picatoste@gmail.com>
2020-11-12 11:48:07 +0100

    Docs añadido

---
+++ docs/precache-manifest.70e58e173d4f198ddee1e3896945124c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74d37c662e99436ee7a78b4f0f63dbd",
+    "url": "/pokemon-game/index.html"
+  },
+  {
+    "revision": "105152f609277f045203",
+    "url": "/pokemon-game/static/css/main.58896bcc.chunk.css"
+  },
+  {
+    "revision": "dddd58b87ea79f36468a",
+    "url": "/pokemon-game/static/js/2.cde9239f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon-game/static/js/2.cde9239f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105152f609277f045203",
+    "url": "/pokemon-game/static/js/main.87e9359a.chunk.js"
+  },
+  {
+    "revision": "4e92f5d6c7b1fc3be01d",
+    "url": "/pokemon-game/static/js/runtime-main.7409d4d6.js"
+  }
+]);
